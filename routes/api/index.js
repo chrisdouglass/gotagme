5c6744b6965929723cef332fa1bdc23567f1d664
Fizzle <loungehound@gmail.com>
2018-01-12 03:56:19 -0800

    Adds db file and a huge comment describing a db structure.

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const router = express.Router();
 
+// DB route for all of that good stuff.
+const db = require('./db');
+router.use('/db', db);
+
 // The flickr API route wraps the flickr API.
 const flickr = require('./flickr');
 router.use('/flickr', flickr);
