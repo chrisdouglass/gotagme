b33667a00ab05454ba487008a4ac143daf69748a
Fizzle <loungehound@gmail.com>
2018-02-13 17:52:23 -0800

    Adds components for the homepage and searchbar.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component'
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SearchbarComponent } from './searchbar/searchbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    HomeComponent,
+    SearchbarComponent,
   ],
   imports: [
     BrowserModule
