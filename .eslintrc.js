8b9ad19948ff1b0cf19bf1d216d6349405ccda5b
Fizzle <loungehound@gmail.com>
2018-02-22 19:31:06 -0800

    Typescript server launching changes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,15 @@ module.exports = {
       "google",
       "plugin:promise/recommended",
     ],
+    "env": {
+      "node": true,
+      "mocha": true,
+      "es6": true
+    },
+    "parser": "typescript-eslint-parser",
     "parserOptions": {
       "ecmaVersion": 8,
+      "sourceType": "module"
     },
     "plugins": [
       "promise",
